da7567348adba0301669072ecdf8e4fa2a4310d0
IJke <ijke@overscope.net>
2018-02-28 14:10:02 +0100

    Add camera node, ShapeType enum, spawnShape function

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -14,12 +14,15 @@ class GameViewController: UIViewController {
     
     var scnView: SCNView!
     var scnScene: SCNScene!
+    var cameraNode: SCNNode!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
         setupScene()
+        setupCamera()
+        spawnShape()
     }
     
     override var shouldAutorotate: Bool {
@@ -37,5 +40,24 @@ class GameViewController: UIViewController {
     func setupScene() {
         scnScene = SCNScene()
         scnView.scene = scnScene
+        
+        scnScene.background.contents = "GeometryFighter.scnassets/Textures/Background_Diffuse.jpg"
+    }
+    
+    func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        scnScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    func spawnShape() {
+        var geometry: SCNGeometry
+        switch ShapeType.random() {
+        default:
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        }
+        let geometryNode = SCNNode(geometry: geometry)
+        scnScene.rootNode.addChildNode(geometryNode)
     }
 }
