36c5bedfd8994aa4fa7070a7178e5765df7d6781
IJke <ijke@overscope.net>
2018-02-28 16:35:06 +0100

    Add GameUtils, add all cases for ShapeType to GameViewController

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -35,6 +35,9 @@ class GameViewController: UIViewController {
     
     func setupView() {
         scnView = self.view as! SCNView
+        scnView.showsStatistics = true
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = true
     }
     
     func setupScene() {
@@ -54,8 +57,22 @@ class GameViewController: UIViewController {
     func spawnShape() {
         var geometry: SCNGeometry
         switch ShapeType.random() {
-        default:
+        case .box:
             geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        case .capsule:
+            geometry = SCNCapsule(capRadius: 1.0, height: 1.0)
+        case .cone:
+            geometry = SCNCone(topRadius: 0.5, bottomRadius: 1.0, height: 1.0)
+        case .cylinder:
+            geometry = SCNCylinder(radius: 0.5, height: 1.0)
+        case .pyramid:
+            geometry = SCNPyramid(width: 1.0, height: 1.0, length: 1.0)
+        case .sphere:
+            geometry = SCNSphere(radius: 1.0)
+        case .torus:
+            geometry = SCNTorus(ringRadius: 0.5, pipeRadius: 1.0)
+        case .tube:
+            geometry = SCNTube(innerRadius: 0.5, outerRadius: 1.0, height: 1.0)
         }
         let geometryNode = SCNNode(geometry: geometry)
         scnScene.rootNode.addChildNode(geometryNode)
